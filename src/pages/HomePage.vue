f3bce9c70aa3041574851117289e8c237fb15b6a
Drew0004 <drdrewgraphic@gmail.com>
2024-01-30 00:20:00 +0100

    Add complete smart tutoring section - section 3 - fix hover

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -65,7 +65,30 @@ export default {
                     lessons: '6',
                     students:'51'
                 },
-            ]
+            ],
+            smartTutoring:[
+                {
+                    image:'../../public/course-portal/images/home-2-image-box-01.png',
+                    title: 'Customize Preferences',
+                    description:'Leaners are given the right to arrange and customize their study schedule and timetable based on preferences.',
+                    link: 'Start here',
+
+                },
+                {
+                    image:'../../public/course-portal/images/about-us-02-image-box-02.png',
+                    title: 'Cloud Storage',
+                    description:'You can store online lessons via online cloud or download to your device and revise lessons on the way.',
+                    link: 'Get Free Quote',
+
+                },
+                {
+                    image:'../../public/course-portal/images/home-2-image-box-03.png',
+                    title: 'Multimedia Channels',
+                    description:'We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.',
+                    link: 'Start now',
+
+                }
+            ],
         };
     },
     methods: {
@@ -149,7 +172,7 @@ export default {
                         <div class="col-7">
 
                             <h4 class="mb-3">{{ singleCourse.price }}</h4>
-                            <h5 class="mb-3">{{ singleCourse.title }}</h5>
+                            <h5 class="mb-3 fw-semibold">{{ singleCourse.title }}</h5>
                             <span class="fw-light me-4"><i class="fa-regular fa-file-lines"></i> {{ singleCourse.lessons }} Lesson</span>
                             <span class="fw-light"><i class="fa-regular fa-user"></i> {{ singleCourse.students }} Students</span>
                         </div>
@@ -163,7 +186,25 @@ export default {
             </div>
 
         </section>
-    
+
+        <!---- Smart tutoring section ---->
+        <section class="smart-tutoring-section py-5">
+            <div class="container">
+                <h4 class="text-center fs-6 mb-3">Maxcoack Available Courses</h4>
+                <h2 class="text-center fw-bold mb-5">Access <span class="fw-light text-highlight">Smart Tutoring</span> Program For benefits.</h2>
+
+                <div class="row">
+                    <div class="tutoring-card" v-for="(singleTutoring, i) in smartTutoring">
+                        <div>
+                            <img :src="singleTutoring.image" :alt="singleTutoring.title">
+                        </div>
+                        <h3 class="fw-semibold my-4">{{ singleTutoring.title }}</h3>
+                        <p class="mb-4">{{ singleTutoring.description }}</p>
+                        <a class="fw-semibold" href="#nogo">{{ singleTutoring.link }} <i class="fa-solid fa-arrow-right-long"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -266,6 +307,12 @@ export default {
             width: calc((100% / 6) - 40px);
             margin: 0 20px;
             object-fit: contain;
+            opacity: 0.5;
+            transition: 0.2s ease-in;
+            
+            &:hover{
+                opacity: 1;
+            }
         }
     }
 
@@ -311,6 +358,14 @@ export default {
                 color: $main-green-color;
                 font-weight: bold;
             }
+            h5{
+                color: $main-purple-color;
+                &:hover{
+                    transition: 0.2s ease-in;
+                    color: $main-green-color;
+                    cursor: pointer;
+                }
+            }
             span{
                 color: $main-gray-color;
                 font-size: 0.9rem;
@@ -331,6 +386,38 @@ export default {
             &:hover{
                 background-color: $main-purple-color;
             }
+        }
+    }
+
+    .smart-tutoring-section{
+        .text-highlight{
+            color: $main-green-color;
+        }
+        h2{
+            color: $main-purple-color;
+        }
+
+        .tutoring-card{
+            width: calc((100% / 3) - 40px);
+            margin: 0 20px;
+            cursor: pointer;
+
+            h3{
+                color: $main-purple-color;
+            }
+            p{
+                color: $main-gray-color;
+            }
+            a{
+                text-decoration: none;
+                color: $main-indigo-color;
+                transition: 0.2s ease-in;
+
+                &:hover{
+                    color: $main-green-color;
+                }
+            }
+
         }
     }
 </style>
