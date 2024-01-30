2c30a5370caa273f01e52000e8e31cd87b20df99
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-01-30 16:11:51 +0100

    Prima pagina Customer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,9 +13,91 @@ export default {
 
 <template>
     <footer>
-        FOOTER
+       <div class="container">
+
+        <section class="Second">
+            <p><h5>Address</h5></p>
+            <div>
+                382 NE 191st St # 87394 Miami, FL 33179-3899
+            </div>
+            <div class="ab">
+                +1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)
+            </div>
+            <div class="ab"></div>
+            <a class="ab">
+                support@maxcoach.com
+            </a>
+        </section>
+
+        <section class="First">
+            <p><h5>Explore</h5></p>
+            <a class="ab" href="http://localhost:5173/QuickStart">Start here</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Blog</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">About us</a>
+        </section>
+
+        <section class="Third">
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Membership</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Privacy policy</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Terms of services</a>
+        </section>
+
+        <section class="First">
+            <p><h5>Information</h5></p>
+            <a class="ab" href="http://localhost:5173/QuickStart">Start here</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Membership</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Privacy policy</a>
+            <div class="ab"></div>
+            <a href="#nogo" class="ab">Terms of services</a>
+        </section>
+       </div>
+
     </footer>
 </template>
 
 <style lang="scss" scoped>
+footer {
+    background-color: lightgray;
+    height: 300px;
+    padding-left: 120px;
+    padding-top: 50px;
+}
+
+.container {
+    display: flex;
+}
+
+.First {
+    padding-left: 70px;
+}
+
+.Second {
+    width: 650px;
+}
+
+.Third {
+    padding-left: 70px;
+    padding-top: 42px;
+}
+
+.ab {
+    padding-top: 15px;
+}
+
+a {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+a:hover {
+    color: green;
+}
 </style>
--- src/pages/CustomerPage.vue
+++ src/pages/CustomerPage.vue
@@ -13,9 +13,124 @@ export default {
 
 <template>
     <main>
-        <h1>Customer</h1>
+        <h1>
+            Customer-centric Info-Tech Strategies
+        </h1>
+
+        <section class="container">
+            <h3>
+                Course Description
+            </h3>
+
+            <p>
+                Now more than ever, corporations are investing heavily in IT. The quality of these investments affects the daily work of millions.
+            </p>
+            <p>
+                Yet it’s not uncommon to see industry surveys where the failure rates for IT projects is over 50%. It’s possible to do better and it’s possible to do so consistently. Use the Business Model Canvas to focus your company strategy and facilitate buy-in from stakeholders.
+            </p>
+
+            <iframe width="720" height="405" src="https://www.youtube.com/embed/m-X1ExlQ9uE?feature=oembed" allowfullscreen=""></iframe>
+            <p>
+                In this two-week course, we’ll step through major challenges within corporate IT and how to address them with the disciplined use of design thinking, Lean Startup, and agile as a team framework.
+            </p>
+            <p>
+                Translate your work on the Canvas to specific charters in IT. Rapidly prototype strategically-aligned processes for implementation within your IT infrastructure. How do you define a customer-centric strategy you can actually execute? To do strategic IT, you’ve got to have a strategy! Ideally, you need one that’s easy to understand and use as a clear basis for action. 
+            </p>
+            <p>
+                That’s what you’ll learn in this course.
+            </p>
+            <h3>
+                A Short Description
+            </h3>
+            <p>
+                A customer-centric way of doing business is a way that provides a positive customer experience before and after the sale in order to drive repeat business, enhance customer loyalty and improve business growth.
+            </p>
+            <p>
+                But, a customer-centric company is more than a company that offers good service. Both Amazon and Zappos are prime examples of brands that are customer-centric and have spent years creating a culture around the customer and their needs. Their commitment to delivering customer value is genuine – In fact, Zappos is happy to fire employees if they do not fit within their customer-centric culture!
+            </p>
+            <p>
+                But, how important is being customer-centric? The good news is that’s becoming very important! Econsultancy recently asked what the most important characteristic is in order to establish a truly “digital-native” culture. The answer to that question and leading the responses with 58% was to be customer-centric. 
+            </p>
+            <p>
+                Enroll in this course to find out.
+            </p>
+        </section>
+
+        <section class="container1">
+            <div class="box">
+            <p>
+                <h5>
+                    Change simplification
+                </h5>
+            </p>
+            <p>
+                <em>General introduction to customer-centric strategies</em>
+            </p>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    30 minutes
+                </button>
+                <button class="green">
+                    
+                </button>
+            </div>
+        </section>
     </main>
 </template>
 
 <style lang="scss" scoped>
+
+main {
+    background-color: rgb(217, 216, 216);
+}
+
+h1 {
+    text-align: center;
+}
+
+.container {
+    background-color: aqua;
+    width: 750px;
+    height: 1700px;
+    margin: 70px;
+    flex-wrap: wrap;
+    border-radius: 5px;
+}
+
+.container1 {
+    background-color: aqua;
+    width: 750px;
+    height: 1700px;
+    margin: 70px;
+    flex-wrap: wrap;
+    border: 1px solid black;
+}
+.box {
+    padding-top: 25px;
+    padding-left: 45px;
+}
+
+.grey {
+    background-color: rgb(209, 209, 209);
+    padding-left: 50px;
+    align-items: center;
+    height: 40px;
+}
+.red {
+    color: rgb(208, 159, 159);
+    width: 100px;
+    border: 0px;
+    border-radius: 5px;
+}
+
+.green {
+    color: rgb(26, 123, 63);
+    width: 100px;
+    border: 0px;
+    border-radius: 5px;
+}
+
 </style>
