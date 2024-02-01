e4da1ab0109ee21f4162323c63c78969c9c31331
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-02-01 17:01:28 +0100

    Second commit

--- src/pages/CustomerPage.vue
+++ src/pages/CustomerPage.vue
@@ -17,6 +17,10 @@ export default {
             Customer-centric Info-Tech Strategies
         </h1>
 
+        <section class="mini-container">
+            Overview
+        </section>
+
         <section class="container">
             <h3>
                 Course Description
@@ -56,6 +60,54 @@ export default {
             </p>
         </section>
 
+        <aside>
+          <div class="little-box">
+            <div class="flex">
+                <i class="fa-solid fa-money-bill" style="padding-top: 5px; padding-right: 5px;"></i><strong>Price</strong>
+                <h3 style="width: 250px; text-align: right; color: green"> Free </h3>
+            </div>
+
+            <div class="flex">
+                <i class="fa-solid fa-chalkboard-user" style="padding-top: 5px; padding-right: 5px;"></i><strong>Instructor</strong>
+                <div style="margin-left: 14px;">Maggie Strickland</div>
+            </div>
+
+            <div class="flex">
+                <i class="fa-regular fa-clock" style="padding-top: 5px; padding-right: 5px;"></i><strong>Duration</strong>
+                <div style="margin-left: 87px;">15 weeks</div>
+            </div>
+
+            <div class="flex">
+                <i class="fa-regular fa-file" style="padding-top: 5px; padding-right: 5px;"></i><strong>Lectures</strong>
+                <div style="margin-left: 142px;">24</div>
+            </div>
+
+            <div class="flex">
+                <i class="fa-regular fa-user" style="padding-top: 5px; padding-right: 5px;"></i><strong>Lectures</strong>
+                <div style="margin-left: 67px;">957 students</div>
+            </div>
+
+            <div class="flex">
+                <i class="fa-solid fa-language" style="padding-top: 5px; padding-right: 5px;"></i><strong>Lectures</strong>
+                <div style="margin-left: 100px;">English</div>
+            </div>
+
+            <div class="flex">
+                <i class="fa-solid fa-calendar-days" style="padding-top: 5px; padding-right: 5px;"></i><strong>Deadline</strong>
+                <div style="margin-left: 70px;">05 Jan 2019</div>
+            </div>
+
+            <div class="flex">
+                <a href="http://localhost:5173/QuickStart"><button class="Start">Start Now</button></a>
+            </div>
+
+            <div>
+                Share this course <i class="fa-solid fa-share-nodes" style="margin-top: 20px; text-align: center; width:"></i>
+            </div>
+          </div>
+
+        </aside>
+
         <section class="container1">
             <div class="box">
             <p>
@@ -74,14 +126,385 @@ export default {
                     30 minutes
                 </button>
                 <button class="green">
-                    
+                    Preview
                 </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-video"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    15 minutes
+                </button>
+                <button class="green">
+                    3 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    50 minutes
+                </button>
+                <button class="green">
+                    0 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    2 hours
+                </button>
+                <button class="green">
+                    Preview
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    1 hour
+                </button>
+                <button class="green">
+                    1 questions
+                </button>
+            </div>
+
+            <div class="grey">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    10 minutes
+                </button>
+                <button class="green">
+                    9 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    30 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="grey">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    10 minutes
+                </button>
+                <button class="green">
+                    3 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    10 minutes
+                </button>
+                <button class="green">
+                    0 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+        </section>
+
+        <section class="container2">
+            <div class="box">
+            <p>
+                <h5>
+                    Customer Advisory Board
+                </h5>
+            </p>
+            <p>
+                <em>Learn about the basics of Customer Advisory Board</em>
+            </p>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    30 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    45 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    50 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    1 hours
+                </button>
+                <button class="green">
+                    5 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+        </section>
+
+        <section class="container2">
+            <div class="box">
+            <p>
+                <h5>
+                    Feedback survey
+                </h5>
+            </p>
+            <p>
+                <em>The major things about conducting a survey and manage feedback</em>
+            </p>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    30 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    45 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="white">
+                Live meeting about Infotech Strategies
+                <button class="red">
+                    50 minutes
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+
+            <div class="grey">
+                Lesson 01: Simple, attainables goal
+                <button class="red">
+                    1 hours
+                </button>
+                <button class="green">
+                    5 questions
+                </button>
+                <a href="http://localhost:5173/Lessons"><i class="fa-solid fa-lock"></i></a>
+            </div>
+        </section>
+
+        <section>
+           <div class="container2">
+            <div class="Instructor">
+                <img src="C:\Users\lucad\OneDrive\Documents\Esercizi\proj-html-vuejs\images\about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" class="foto">
+                <div class="informations">
+                    <h4>
+                        Maggie Strickland
+                    </h4>
+                    <p>/Advanced Educator</p>
+                    <p>Maggie is a brilliant educator, whose life was spent for computer science and love of nature. Being a female, she encountered a lot of obstacles and was forbidden to work in this field by her family. With a true spirit and talented gift, she was able to succeed and set an example for others.</p>
+                    <div class="icons">
+                        <a href="http://localhost:5173/QuickStart"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="http://localhost:5173/QuickStart" style="margin-left: 10px;"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="http://localhost:5173/QuickStart" style="margin-left: 10px;"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="http://localhost:5173/QuickStart" style="margin-left: 10px;"><i class="fa-brands fa-pinterest"></i></a>
+                        <a href="http://localhost:5173/QuickStart" style="margin-left: 10px;"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+           </div>
+        </section>
+
+        <section>
+            <div class="container2">
+                <h4>
+                    Review
+                </h4>
+                <div class="flex">
+                    Average rating
+                    <div style="margin-left: 110px;">
+                        Detailed rating
+                    </div>
+                </div>
+
+                <div>
+
+                <div class="flex">
+
+                <div class="white-box">
+                    <h1 style="color: green;">4.50</h1>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="margin-left: 5px;"></i>
+                    <p>(2 rating)</p>
+                </div>
+
+                <div>
+                <div style="padding-top: 50px; padding-left: 50px;" class="flex">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <div class="line-white"><div class="line-green"></div></div>
+                </div>
+
+                <div style="padding-top: 10px; padding-left: 50px;" class="flex">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <div class="line-white"><div class="line-green"></div></div>
+                </div>
+
+                <div style="padding-top: 10px; padding-left: 50px;" class="flex">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <div class="line-white"></div>
+                </div>
+
+                <div style="padding-top: 10px; padding-left: 50px;" class="flex">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <div class="line-white"></div>
+                </div>
+
+                <div style="padding-top: 10px; padding-left: 50px;" class="flex">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <i class="fa-regular fa-star" style="margin-left: 5px;"></i>
+                    <div class="line-white"></div>
+                </div>
+
+                </div>
+                
+                </div>
+
+                </div>
+
+                    <div class="recensione">
+                      <div class="flex">
+                        <img src="C:\Users\lucad\OneDrive\Documents\Esercizi\proj-html-vuejs\images\about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" class="recensione-foto">
+                    <div>
+                        <div style="padding-left: 50px;" class="flex">
+                            <strong>EDNAWATSON</strong>
+                        </div>
+
+                        <div style="padding-left: 50px; padding-top: 10px;">
+                            <strong>Cover all my needs</strong>
+                        </div>
+
+                        <div style="padding-left: 50px; padding-top: 10px;">
+                            The course identify things we want to change and then figure out the things that need to be done to create the desired outcome. The course helped me in clearly define problems and generate a wider variety of quality solutions. Support more structures analysis of options leading to better decisions.
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
 </template>
 
 <style lang="scss" scoped>
+
+.Start {
+    background-color: rgb(71, 153, 71);
+    width: 250px;
+    height: 50px;
+    text-align: center;
+    color: white;
+    border-radius: 10px;
+}
+
+.recensione {
+    margin-top: 50px;
+}
+
+.recensione-foto {
+    border-radius: 50%;
+    width: 155px;
+    height: 155px;
+}
+
+.line-green {
+    background-color: green;
+    height: 10px;
+    width: 150px;
+    border-radius: 3px;
+}
+
+.line-white {
+    background-color: lightgrey;
+    height: 10px;
+    width: 300px;
+    margin-top: 5px;
+    margin-left: 15px;
+    border-radius: 3px;
+}
+
+.white-box {
+    width: 165px;
+    height: 165px;
+    margin: 1px black;
+    background-color: white;
+    margin-top: 25px;
+    padding: 25px;
+    text-align: center;
+}
+
+.Start:hover {
+    background-color: rgb(190, 77, 190);
+}
+
+aside > .little-box > .flex {
+    padding-top: 20px;
+}
+
+.flex {
+    display: flex;
+}
 
 main {
     background-color: rgb(217, 216, 216);
@@ -91,9 +514,22 @@ h1 {
     text-align: center;
 }
 
+
+
+.informations {
+    margin-left: 150px;
+}
+
+.mini-container {
+    background-color: aqua;
+    width: 800px;
+    padding: 20px;
+    margin-left: 70px;
+    
+}
 .container {
     background-color: aqua;
-    width: 750px;
+    width: 800px;
     height: 1700px;
     margin: 70px;
     flex-wrap: wrap;
@@ -102,11 +538,26 @@ h1 {
 
 .container1 {
     background-color: aqua;
-    width: 750px;
+    width: 800px;
     height: 1700px;
     margin: 70px;
     flex-wrap: wrap;
     border: 1px solid black;
+}
+
+.container2 {
+    background-color: aqua;
+    width: 800px;
+    height: 700px;
+    margin: 70px;
+    flex-wrap: wrap;
+    border-radius: 5px;
+    padding: 50px;
+}
+
+.Instructor {
+    width: 750px;
+    display: flex;
 }
 .box {
     padding-top: 25px;
@@ -114,7 +565,13 @@ h1 {
 }
 
 .grey {
-    background-color: rgb(209, 209, 209);
+    background-color: rgb(233, 232, 232);
+    padding-left: 50px;
+    align-items: center;
+    height: 40px;
+}
+.white {
+    background-color: white;
     padding-left: 50px;
     align-items: center;
     height: 40px;
@@ -124,13 +581,31 @@ h1 {
     width: 100px;
     border: 0px;
     border-radius: 5px;
+    margin-left: 180px;
 }
 
 .green {
-    color: rgb(26, 123, 63);
+    color: white;
     width: 100px;
     border: 0px;
     border-radius: 5px;
+    background-color: rgb(68, 181, 115);
+    margin-left: 10px;
+}
+
+.foto {
+    width: 200px;
+    height: 200px;
+}
+
+aside {
+    width: 300px;
+    background-color: aqua;
+    height: 66vh;
+    position: fixed;
+    top: 180px;
+    right: 100px;
+    padding: 30px;
 }
 
 </style>
